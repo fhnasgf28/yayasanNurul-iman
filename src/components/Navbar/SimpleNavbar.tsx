2f5dfa6164499ba755082c5d6d0e8d8d17d3a7e8
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2023-07-28 22:31:10 +0700

    memperbaiki eslint yang error

--- src/components/Navbar/SimpleNavbar.tsx
+++ src/components/Navbar/SimpleNavbar.tsx
@@ -47,6 +47,8 @@ const SimpleNavbar = ({ openSearch, changeTheme, toggleSideMenu, openSidebar = f
                   {each.label}
                 </a>
                 :
+                
+                // eslint-disable-next-line react/jsx-key
                 <NavCatergoryDD label={each.label} openDD={openDD} setOpenDD={() => setOpenDD(!openDD)} floating />
             ))
           }
